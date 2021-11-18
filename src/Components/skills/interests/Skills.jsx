687a2dee6051d88a7906732f787c0ee4a7d10250
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-18 19:02:59 +0100

    .

--- src/Components/skills/interests/Skills.jsx
+++ src/Components/skills/interests/Skills.jsx
@@ -8,10 +8,10 @@ const Skills = () => {
         <div className="skill_title justify-content-between">
           <h6 className="block-title">Skills & endorsements</h6>
           <div className="d-none d-md-block ml-auto add-skill">
-            Add a new skill </div>
+            Add a new skill{" "}
+          </div>
           <div className="mb-2 ml-3 edit-logo">
             <svg
-
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               data-supported-dps="24x24"
@@ -21,8 +21,8 @@ const Skills = () => {
               focusable="false"
             >
               <path d="M21.13 2.86a3 3 0 00-4.17 0l-13 13L2 22l6.19-2L21.13 7a3 3 0 000-4.16zM6.77 18.57l-1.35-1.34L16.64 6 18 7.35z"></path>
-            </svg></div>
-
+            </svg>
+          </div>
         </div>
         <div>
           <Button className="btn btn-primary btn-skill px-3">
@@ -117,7 +117,6 @@ const Skills = () => {
                   2 of Hedri Helena’s colleagues at Princess Cruises
                 </span>{" "}
               </div>
-
             </div>
           </div>
         </div>
@@ -153,7 +152,6 @@ const Skills = () => {
                   2 of Stefano’s colleagues at Nucleode Srl
                 </span>{" "}
               </div>
-
             </div>
           </div>
         </div>
