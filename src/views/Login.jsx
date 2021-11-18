d6bb6ddfae35b6079b1bbf0a12f81247154f6c30
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-18 20:22:34 +0100

    .

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -252,10 +252,7 @@ function Login(props) {
         </div>
 
         <div className="container d-flex flex-column justify-content-center align-items-center mod-logo-maxWidth">
-          <h5 className="mb-4">
-            Don't have an account? Sign up or use the details below
-          </h5>
-          <h5 className="mb-4">username: Yon, password: 123456789</h5>
+          <h5 className="mb-4">Don't have an account?</h5>
           <button
             id="btn-sign-up-strivefy"
             className="btn btn-outline-dark btn-large w-100"
@@ -263,6 +260,7 @@ function Login(props) {
           >
             SIGN UP FOR LINKEDIN
           </button>
+          <small className="mb-4">username: Yon, password: 123456789</small>
         </div>
 
         <div className="container mod-logo-maxWidth mt-4">
